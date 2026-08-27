03ee51b377295b8619c1b2a6de324e83323cfd48
Your Name <you@example.com>
2026-08-27 09:48:37 -0400

    feat:creacion de una nueva prueba

--- src/fizzbuzzz.spec.js
+++ src/fizzbuzzz.spec.js
@@ -7,6 +7,9 @@ describe("FizzBuzz", () => {
   it("deberia generar el mismo número si no sigue una regla", () => {
     expect(fizzBuzz(1)).toEqual("1");
   });
+  it("deberia retornar 'Fizz' si el número es 3 o múltiplo de 3", () => {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+  });
 });
 
 
